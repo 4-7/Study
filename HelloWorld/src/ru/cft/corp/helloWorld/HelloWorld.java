c5aac15085a0bd0f07bde558800eab5147c0ecb7
4-7 <d.s.elistratov@mail.ru>
2021-10-17 14:03:27 +0500

    First Change.

--- HelloWorld/src/ru/cft/corp/helloWorld/HelloWorld.java
+++ HelloWorld/src/ru/cft/corp/helloWorld/HelloWorld.java
@@ -1,10 +1,8 @@
 package ru.cft.corp.helloWorld;
 
-import org.springframework.context.ApplicationContext;
-
 public class HelloWorld {
 	public static void Main{
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+		ApplicationContext context = new ClassPathXmlApplicationContext("");
 
 		context.close();
 	}
